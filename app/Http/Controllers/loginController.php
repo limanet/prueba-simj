56cdd64568771c5bda65866446c073c59ce52882
Aníbal Liñán Martínez <anibal@limanet.es>
2024-06-24 18:27:25 +0200

    Creado sistema de login

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class loginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate( [
+            'email' => 'required|email',
+            'password' => 'required'
+        ] );
+
+        $remember = ( $request->remember ) ? true : false;
+
+        if (Auth::attempt($credentials, $remember)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors( [ 'failed' => 'E-mail y/o contraseña incorrectos.' ] )->withInput();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rutas para el login
+Route::view( 'login', 'auth.login' );
+Route::post( 'login', [ \App\Http\Controllers\loginController::class, 'login' ] )->name( 'login' );
+
+Route::get( '/', function () {
+    return view( 'welcome' );
+} )->middleware( 'auth' )->name( 'index' );
