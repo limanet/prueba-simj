538a9304abb6bdadb45d892ac04ff0bcdb361666
Aníbal Liñán Martínez <anibal@limanet.es>
2024-07-11 06:16:04 +0200

    Creada migración para la creación de la tabla que va a contener la info de los días festivos

---
+++ database/migrations/2024_07_11_040755_create_calendar_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->id( 'id_calendar' );
+            $table->integer( 'day' );
+            $table->integer( 'month' );
+            $table->integer( 'year' );
+            $table->string( 'name' );
+            $table->string( 'color' );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('calendar');
+    }
+};
